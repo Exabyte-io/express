b47d6c83096f6532aacff18383ea130d4db42b46
Mohammad Mohammadi <mohammad@exabyte.io>
2017-08-01 19:12:31 -0700

    Bumped version

--- setup.py
+++ setup.py
@@ -13,10 +13,11 @@ setup(
         "bunch==1.0.1",
         "numpy==1.10.4",
         "xmltodict==0.9.2",
-        "esse==0.1.0"
+        "esse==0.1.0",
+        "pymatgen==4.2.1"
     ],
     dependency_links=[
-        "git+https://git@github.com/Exabyte-io/exabyte-materials-json@258e4de02c1684090df7e451663f81079b80c19b#egg=esse-0.1.0"
+        "git+https://git@github.com/Exabyte-io/exabyte-materials-json@5aa903f34abe93c59af46ee49d1f9614eb1f9081#egg=esse-0.1.0"
     ],
     classifiers=[
         'Programming Language :: Python',
