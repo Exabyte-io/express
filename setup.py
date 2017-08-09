cb5551a42b11a933adc7b87f3e0fc88fc1439be6
Mohammad Mohammadi <mohammad@exabyte.io>
2017-08-09 14:57:33 -0700

    Bumped version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         "pymatgen==4.2.1"
     ],
     dependency_links=[
-        "git+file:///stack/lib/esse@9454fd081426676ac299807bf3fde543a414dc03#egg=esse-0.1.0"
+        "git+file:///stack/lib/esse@10c7a1d054e5ec8e610cbd50f19ee0c9bf80208d#egg=esse-0.1.0"
     ],
     classifiers=[
         'Programming Language :: Python',
