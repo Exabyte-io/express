5cf90c8a6d2652df8c66d26a594e1cfb1c8fc3d5
Mohammad Mohammadi <mohammad@exabyte.io>
2017-08-20 10:29:12 -0700

    renamed get_schema_default_values to get_property_default_values

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         "pymatgen==4.2.1"
     ],
     dependency_links=[
-        "git+file:///stack/lib/esse@78a1e65607e421bb34805f51264fec9308ed7692#egg=esse-0.1.0"
+        "git+file:///stack/lib/esse@044a14a3125bc5fab7ef5262c215e05bb4cba288#egg=esse-0.1.0"
     ],
     classifiers=[
         'Programming Language :: Python',
