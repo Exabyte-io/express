2f834801fe339bbb0e78cf2cb67dee9a08309b49
Mohammad Mohammadi <mohammad@exabyte.io>
2017-08-21 11:55:49 -0700

    fixed lattice and basis units

--- tests/integration/parsers/apps/espresso/test_parser.py
+++ tests/integration/parsers/apps/espresso/test_parser.py
@@ -1,6 +1,60 @@
 from tests.integration import IntegrationTestBase
 from express.parsers.apps.espresso.parser import EspressoParser
 
+BASIS = {
+    "units": "angstrom",
+    "elements": [
+        {
+            "id": 1,
+            "value": "Si "
+        },
+        {
+            "id": 2,
+            "value": "Si "
+        }
+    ],
+    "coordinates": [
+        {
+            "id": 1,
+            "value": [
+                0.0,
+                0.0,
+                0.0
+            ]
+        },
+        {
+            "id": 2,
+            "value": [
+                1.1159329259917776,
+                0.789083666533198,
+                1.9328525178272928
+            ]
+        }
+    ]
+}
+
+LATTICE = {
+    "units": "angstrom",
+    "vectors": {
+        "a": [
+            3.3477985280590525,
+            0.0,
+            1.9328525178272928
+        ],
+        "c": [
+            0.0,
+            0.0,
+            3.8657050356545857
+        ],
+        "b": [
+            1.1159331759080586,
+            3.1563346661327913,
+            1.9328525178272928
+        ],
+        "alat": 1.0
+    }
+}
+
 
 class TestEspressoParser(IntegrationTestBase):
     def setUp(self):
@@ -18,3 +72,9 @@ class TestEspressoParser(IntegrationTestBase):
 
     def test_espresso_pressure(self):
         self.assertAlmostEqual(self.parser.pressure(), 73.72, places=2)
+
+    def test_espresso_basis(self):
+        self.assertDeepAlmostEqual(self.parser.basis(), BASIS, places=2)
+
+    def test_espresso_lattice_vectors(self):
+        self.assertDeepAlmostEqual(self.parser.lattice_vectors(), LATTICE, places=2)
--- tests/integration/parsers/apps/vasp/test_parser.py
+++ tests/integration/parsers/apps/vasp/test_parser.py
@@ -1,6 +1,60 @@
 from tests.integration import IntegrationTestBase
 from express.parsers.apps.vasp.parser import VaspParser
 
+BASIS = {
+    "units": "angstrom",
+    "elements": [
+        {
+            "id": 0,
+            "value": "Si"
+        },
+        {
+            "id": 1,
+            "value": "Si"
+        }
+    ],
+    "coordinates": [
+        {
+            "id": 0,
+            "value": [
+                0.0,
+                0.0,
+                0.0
+            ]
+        },
+        {
+            "id": 1,
+            "value": [
+                1.1163067500000001,
+                0.789348,
+                1.9335
+            ]
+        }
+    ]
+}
+
+LATTICE = {
+    "vectors": {
+        "a": [
+            3.34892,
+            0.0,
+            1.9335
+        ],
+        "units": "angstrom",
+        "c": [
+            0.0,
+            0.0,
+            3.867
+        ],
+        "b": [
+            1.116307,
+            3.157392,
+            1.9335
+        ],
+        "alat": 1.0
+    }
+}
+
 
 class TestVaspParser(IntegrationTestBase):
     def setUp(self):
@@ -18,3 +72,9 @@ class TestVaspParser(IntegrationTestBase):
 
     def test_vasp_pressure(self):
         self.assertAlmostEqual(self.parser.pressure(), -93.51, places=2)
+
+    def test_vasp_basis(self):
+        self.assertDeepAlmostEqual(self.parser.basis(), BASIS, places=2)
+
+    def test_vasp_lattice_vectors(self):
+        self.assertDeepAlmostEqual(self.parser.lattice_vectors(), LATTICE, places=2)
