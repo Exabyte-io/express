09d889bbad48d027549b9a133479dfe185234ba8
Mohammad Mohammadi <mohammad@exabyte.io>
2017-08-22 13:30:31 -0700

    Moved raw_data and references to separate files

--- tests/unit/properties/non_scalar/test_atomic_forces.py
+++ tests/unit/properties/non_scalar/test_atomic_forces.py
@@ -1,4 +1,5 @@
 from tests.unit import UnitTestBase
+from tests.unit.properties.raw_data import ATOMIC_FORCES_RAW_DATA
 from express.properties.non_scalar.atomic_forces import AtomicForces
 
 ATOMIC_FORCES = {
@@ -33,25 +34,5 @@ class AtomicForcesTest(UnitTestBase):
         super(AtomicForcesTest, self).setUp()
 
     def test_atomic_forces(self):
-        raw_data = {
-            "atomic_forces": [
-                {
-                    "id": 1,
-                    "value": [
-                        -3.9e-07,
-                        -2.4e-07,
-                        0.0
-                    ]
-                },
-                {
-                    "id": 2,
-                    "value": [
-                        3.9e-07,
-                        2.4e-07,
-                        0.0
-                    ]
-                }
-            ]
-        }
-        property_ = AtomicForces("atomic_forces", raw_data=raw_data)
+        property_ = AtomicForces("atomic_forces", raw_data=ATOMIC_FORCES_RAW_DATA)
         self.assertDeepAlmostEqual(property_.serialize_and_validate(), ATOMIC_FORCES)
