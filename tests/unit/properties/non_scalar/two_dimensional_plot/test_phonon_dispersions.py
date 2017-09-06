a22954cd474aeaf02d615334533125347a06da19
Mohammad Mohammadi <mohammad@exabyte.io>
2017-09-06 15:49:47 -0700

    Added phonon_dos and phonon_dispersions tests

---
+++ tests/unit/properties/non_scalar/two_dimensional_plot/test_phonon_dispersions.py
@@ -0,0 +1,16 @@
+from tests.unit import UnitTestBase
+from tests.unit.properties.raw_data import PHONON_DISPERSIONS_RAW_DATA
+from express.properties.non_scalar.two_dimensional_plot.phonon_dispersions import PhononDispersions
+from tests.unit.properties.non_scalar.two_dimensional_plot.phonon_dispersions import PHONON_DISPERSIONS
+
+
+class PhononDispersionsTest(UnitTestBase):
+    def setUp(self):
+        super(PhononDispersionsTest, self).setUp()
+
+    def tearDown(self):
+        super(PhononDispersionsTest, self).setUp()
+
+    def test_phonon_dispersions(self):
+        property_ = PhononDispersions("phonon_dispersions", raw_data=PHONON_DISPERSIONS_RAW_DATA)
+        self.assertDeepAlmostEqual(property_.serialize_and_validate(), PHONON_DISPERSIONS)
