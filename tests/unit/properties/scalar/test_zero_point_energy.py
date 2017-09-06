c2d0613ff6f2ae5405b187d42e9394e5057afd31
Mohammad Mohammadi <mohammad@exabyte.io>
2017-09-05 20:36:57 -0700

    Added test for zero_point_energy

---
+++ tests/unit/properties/scalar/test_zero_point_energy.py
@@ -0,0 +1,20 @@
+from tests.unit import UnitTestBase
+from express.properties.scalar.zero_point_energy import ZeroPointEnergy
+
+ZERO_POINT_ENERGY = {
+    "units": "eV",
+    "name": "zero_point_energy",
+    "value": 1
+}
+
+
+class TotalZeroPointTest(UnitTestBase):
+    def setUp(self):
+        super(TotalZeroPointTest, self).setUp()
+
+    def tearDown(self):
+        super(TotalZeroPointTest, self).setUp()
+
+    def test_zero_point_energy(self):
+        property_ = ZeroPointEnergy("zero_point_energy", raw_data={"zero_point_energy": 1})
+        self.assertDeepAlmostEqual(property_.serialize_and_validate(), ZERO_POINT_ENERGY)
