636700fc7746db4e9a8a346e1e253c7eb41870fc
Mohammad Mohammadi <mohammad@exabyte.io>
2019-03-12 15:48:56 -0700

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='express',
-    version='0.1.0',
+    version='1.0.0',
     description='Exabyte Property Ex(ss)tractor, Sourcer, Serializer class.',
     url='https://github.com/Exabyte-io/exabyte-express',
     author='Exabyte Inc.',
