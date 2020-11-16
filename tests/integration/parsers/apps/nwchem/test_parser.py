18ea6a219f4546cc8bb84d1e9197d47252f7a2b0
Amanda Dewyer <amanda@exabyte.io>
2020-11-15 20:45:29 -0800

    removed nwchem basis test, needs more thought to convert cartesians

--- tests/integration/parsers/apps/nwchem/test_parser.py
+++ tests/integration/parsers/apps/nwchem/test_parser.py
@@ -14,8 +14,5 @@ class TestNwchemParser(IntegrationTestBase):
     def test_nwchem_total_energy(self):
         self.assertAlmostEqual(self.parser.total_energy(), TOTAL_ENERGY, places=2)
 
-    def test_nwchem_basis(self):
-        self.assertDeepAlmostEqual(self.parser.basis(), BASIS, places=2)
-
     def test_nwchem_total_energy_contributions(self):
         self.assertDeepAlmostEqual(self.parser.total_energy_contributions(), TOTAL_ENERGY_CONTRIBUTION, places=2)
