5625c4ec993d37b257f6ec62ccb58d555ed4c98c
Amanda Dewyer <amanda@exabyte.io>
2020-11-16 21:08:33 -0800

    integration testing

--- tests/integration/parsers/apps/nwchem/test_parser.py
+++ tests/integration/parsers/apps/nwchem/test_parser.py
@@ -12,6 +12,8 @@ class TestNwchemParser(IntegrationTestBase):
         super(TestNwchemParser, self).setUp()
 
     def test_nwchem_total_energy(self):
+        print("TOTAL_ENERGY={}".format(TOTAL_ENERGY))
+        print("total_energy={}".format(self.parser.total_energy()))
         self.assertAlmostEqual(self.parser.total_energy(), TOTAL_ENERGY, places=2)
 
     def test_nwchem_total_energy_contributions(self):
