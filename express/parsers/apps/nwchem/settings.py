c67286066afeac1464ed7abfa2c1281de04e0d94
Amanda Dewyer <amanda@exabyte.io>
2020-11-16 19:15:36 -0800

    updated nwchem parser settings

--- express/parsers/apps/nwchem/settings.py
+++ express/parsers/apps/nwchem/settings.py
@@ -7,7 +7,7 @@ COMMON_REGEX = r"{0}\s+[=:<>]\s*([-+]?\d*\.?\d*([Ee][+-]?\d+)?)"
 DOUBLE_REGEX = GENERAL_REGEX.double_number
 
 REGEX = {
-    "total": {
+    "total_energy": {
         "regex": r"Total DFT energy\s+=\s+(\d+)",
         "output_type": "float",
     }
