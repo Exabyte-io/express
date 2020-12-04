b8e9d9b26cd7208cd7b6371ae77350c2880a5477
Amanda Dewyer <amanda@exabyte.io>
2020-12-04 08:29:46 -0800

    moved references from lfs to git

--- tests/fixtures/nwchem/references.py
+++ tests/fixtures/nwchem/references.py
@@ -1,3 +1,67 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f07b253c4c965e95e174b6b7f5cb58b8c361bac7718db0df81f9b9811c721362
-size 1393
+# Reference energies are in units of eV.
+# NWChem returns energies in hartrees, ExPrESS converts to eV
+# Reference energies are all in eV
+TOTAL_ENERGY = -2079.18666382721904
+
+TOTAL_ENERGY_CONTRIBUTION = {
+    "one_electron": {
+        "name": "one_electron",
+        "value": -3350.531714067630674
+    },
+    "coulomb": {
+        "name": "coulomb",
+        "value": 1275.68347728573713
+    },
+    "exchange_correlation": {
+        "name": "exchange_correlation",
+        "value": -254.54658374762781
+    },
+    "nuclear_repulsion": {
+        "name": "nuclear_repulsion",
+        "value": 250.20815670232923
+    }
+}
+
+BASIS = {
+    "units": "angstrom",
+    "elements": [
+        {
+            "id": 1,
+            "value": "O"
+        },
+        {
+            "id": 2,
+            "value": "H"
+        },
+        {
+            "id": 3,
+            "value": "H"
+        }
+    ],
+    "coordinates": [
+        {
+            "id": 1,
+            "value": [
+                0.00000000,
+                0.00000000,
+                0.22143053
+            ]
+        },
+        {
+            "id": 2,
+            "value": [
+                0.00000000,
+                1.43042809,
+                -0.88572213
+            ]
+        },
+        {
+            "id": 3,
+            "value": [
+                0.00000000,
+                -1.43042809,
+                -0.88572213
+            ]
+        }
+    ]
+}
