e9fcffa53d4e3390c86b18d1fdbc6c946e70d569
Simon Adorf <simon.adorf@epfl.ch>
2021-01-28 18:14:13 +0100

    Add doc-strings to aiida parser integration tests definition.

--- tests/integration/parsers/apps/aiida/test_parser.py
+++ tests/integration/parsers/apps/aiida/test_parser.py
@@ -9,9 +9,23 @@ class TestAiidaArchiveParser(IntegrationTestBase):
         self.parser = AiidaArchiveParser(workDir=self.workDir)
 
     def test_aiida_num_structures(self):
+        """
+        Test number of structures parsed from fixture is as expected.
+
+        The fixture contains 39 structures, so the number of parsed
+        structures should match that number.
+        """
+
         self.assertEqual(len(self.parser.structures()), 39)
 
     def test_aiida_expected_keys(self):
+        """
+        Test that all structures parsed from fixture have expected keys.
+
+        All structures parsed from the fixture should contain at least
+        a unit cell definition via 'basis' and 'lattice' vectors.
+        """
+
         structures = self.parser.structures()
         for structure in self.parser.structures():
             for key in ('basis', 'lattice'):
