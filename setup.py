8562c8f836554b20446fbe06007f0d66cee600b3
Amanda Dewyer <amanda@exabyte.io>
2021-02-19 12:04:10 -0800

    updated numpy version

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ setup(
     packages=find_packages(exclude=["tests.*", "tests"]),
     install_requires=[
         "munch==2.5.0",
-        "numpy==1.17.3",
-        # other people use "numpy==1.8.4"
+        "numpy>=1.19.5",
         "pymatgen==2020.4.29",
         "ase==3.17.0",
         "esse>=2020.10.19",
