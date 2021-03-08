7db9f33ef3102e6488a3f925861d3285b5d97b68
James Dean <james@exabyte.io>
2021-03-08 13:37:50 -0800

    Add type hints to init, refactor property method to improve clarity

--- express/__init__.py
+++ express/__init__.py
@@ -2,6 +2,7 @@ import warnings
 import importlib
 
 from express import settings
+from properties import BaseProperty
 
 # disable pymatgen warnings
 warnings.filterwarnings("ignore")
@@ -26,7 +27,7 @@ class ExPrESS(object):
     def __init__(self, parser_name=None, *args, **kwargs):
         self.parser = self._get_parser_class(parser_name)(*args, **kwargs) if parser_name else None
 
-    def _get_parser_class(self, parser_name):
+    def _get_parser_class(self, parser_name: str):
         """
         Returns parser class for a given parser name.
 
@@ -39,7 +40,7 @@ class ExPrESS(object):
         reference = settings.PARSERS_REGISTRY[parser_name]
         return self._get_class_by_reference(reference)
 
-    def _get_class_by_reference(self, reference):
+    def _get_class_by_reference(self, reference: str):
         """
         Returns class by reference.
 
@@ -53,7 +54,7 @@ class ExPrESS(object):
         module_name = '.'.join(reference.split('.')[:-1])
         return getattr(importlib.import_module(module_name), class_name)
 
-    def _get_property_class(self, property_name):
+    def _get_property_class(self, property_name: str):
         """
         Returns property class for a given property name.
 
@@ -66,7 +67,7 @@ class ExPrESS(object):
         reference = settings.PROPERTIES_MANIFEST[property_name]["reference"]
         return self._get_class_by_reference(reference)
 
-    def property(self, property_name, *args, **kwargs):
+    def property(self, property_name: str, *args, **kwargs) -> dict:
         """
         Extracts a given property and validates it against its schema.
 
@@ -78,5 +79,6 @@ class ExPrESS(object):
         Returns:
              dict
         """
-        property_instance = self._get_property_class(property_name)(property_name, self.parser, *args, **kwargs)
+        Property_Class = self._get_property_class(property_name)
+        property_instance : BaseProperty = Property_Class(property_name, self.parser, *args, **kwargs)
         return property_instance.serialize_and_validate()
