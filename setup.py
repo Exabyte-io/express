7bd196faa41b33ba042a8d239367bf374b7871e5
James Dean <james@exabyte.io>
2021-03-11 21:57:01 -0800

    Bump version string

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./README.md', 'r') as f:
 
 setup(
     name='express-py',
-    version='2021.03.15rc3',
+    version='2021.03.15rc4',
     description='Exabyte Property Ex(ss)tractor, Sourcer, Serializer class.',
     long_description=long_description,
     long_description_content_type='text/markdown',
