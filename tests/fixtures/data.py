c26df3dbaad2eab0d49108b57b14949852c65cd1
Amanda Dewyer <amanda@exabyte.io>
2021-06-09 19:46:44 -0700

    fixed data.py error

--- tests/fixtures/data.py
+++ tests/fixtures/data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0f58c277c8232d5d2cff941384815f24cd67d1d919161ebc1e016b858fc023d
-size 35428
+oid sha256:293990dbd6cd46a4d783c55660c5397b4de15050d9b8e6b779a31b45ff999278
+size 35856
