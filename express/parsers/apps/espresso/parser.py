8056d797309302dc5b581271fdda4cfe50d67bbf
James Dean <24254612+AcylSilane@users.noreply.github.com>
2021-12-13 11:34:10 -0800

    refactor(espresso): Refactor `find_xml_file` for readability

--- express/parsers/apps/espresso/parser.py
+++ express/parsers/apps/espresso/parser.py
@@ -43,11 +43,15 @@ class EspressoLegacyParser(BaseParser, IonicDataMixin, ElectronicDataMixin, Reci
             str
         """
         is_sternheimer_gw = self._is_sternheimer_gw_calculation()
+        is_not_sternheimer_gw = not is_sternheimer_gw
+
         for root, dirs, files in os.walk(self.work_dir, followlinks=True):
-            for file_ in [f for f in files if self.xml_data_file_name == f]:
-                file_path = os.path.join(root, file_)
-                if not is_sternheimer_gw or (is_sternheimer_gw and settings.STERNHEIMER_GW0_DIR_PATTERN in file_path):
+            for file in filter(lambda filename: filename == self.xml_data_file_name, files):
+                file_path = os.path.join(root, file)
+                has_found_sternheimer_gw_dir = settings.STERNHEIMER_GW0_DIR_PATTERN in file_path
+                if is_not_sternheimer_gw or (is_sternheimer_gw and has_found_sternheimer_gw_dir):
                     return file_path
+
 
     def total_energy(self) -> float:
         """
