3bc6e6ebfd020a942661d0da59e7d04a4c331623
James Dean <24254612+AcylSilane@users.noreply.github.com>
2021-12-13 17:30:11 -0800

    test(espresso): Remove unneeded import

--- tests/fixtures/espresso/references.py
+++ tests/fixtures/espresso/references.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e34b5142325eb43c4c01a0a6706936b8553c098a694ecd0097a9c2fa9a057e9
-size 19741
+oid sha256:4ac2a16942e71f034efd415c35e478db2c3d0a1eea18cdefcf2f47cb33c9074a
+size 19671
