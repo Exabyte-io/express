4d132110cbef569464d59a65e71d886056e3da76
James Dean <24254612+AcylSilane@users.noreply.github.com>
2021-12-13 18:47:26 -0800

    test(espresso): Remove NOT_TESTED markers since they're no longer needed

--- tests/fixtures/espresso/references.py
+++ tests/fixtures/espresso/references.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d6086fd9f27000325bbbd2e6c113ac3dce1e71959d4a1956b2c1a7e169ecaa8
-size 22529
+oid sha256:2bd58370a644c0ce04998d3078534e0fa474176d32e429ab005cb44fccbb8762
+size 17478
--- tests/integration/parsers/apps/espresso/test_espresso_parser.py
+++ tests/integration/parsers/apps/espresso/test_espresso_parser.py
@@ -47,115 +47,101 @@ class TestEspressoParser(IntegrationTestBase):
     def test_total_energy(self, runtype, test_config):
         test_property = "total_energy"
         parser, reference = self._get_parser_and_reference(runtype, test_config, test_property)
-        if reference != "NOT_TESTED":
-            result = parser.total_energy()
-            self.assertAlmostEqual(reference, result, places=2)
+        result = parser.total_energy()
+        self.assertAlmostEqual(reference, result, places=2)
 
     @for_all_espresso
     def test_fermi_energy(self, runtype, test_config):
         test_property = "fermi_energy"
         parser, reference = self._get_parser_and_reference(runtype, test_config, test_property)
-        if reference != "NOT_TESTED":
-            result = parser.fermi_energy()
-            self.assertAlmostEqual(reference, result, places=2)
+        result = parser.fermi_energy()
+        self.assertAlmostEqual(reference, result, places=2)
 
     @for_all_espresso
     def test_nspin(self, runtype, test_config):
         test_property = "nspin"
         parser, reference = self._get_parser_and_reference(runtype, test_config, test_property)
-        if reference != "NOT_TESTED":
-            result = parser.nspins()
-            self.assertEqual(reference, result)
+        result = parser.nspins()
+        self.assertEqual(reference, result)
 
     @legacy_only
     def test_eigenvalues_at_kpoints(self, runtype, test_config):
         test_property = "eigenvalues_at_kpoints"
         parser, reference = self._get_parser_and_reference(runtype, test_config, test_property)
-        if reference != "NOT_TESTED":
-            result = parser.eigenvalues_at_kpoints()
-            # This test is a special case, because only the first eigenvalue was historically checked
-            first_kpoint_result = result[0]
-            self.assertDeepAlmostEqual(reference, first_kpoint_result)
+        result = parser.eigenvalues_at_kpoints()
+        # This test is a special case, because only the first eigenvalue was historically checked
+        first_kpoint_result = result[0]
+        self.assertDeepAlmostEqual(reference, first_kpoint_result)
 
     @legacy_only
     def test_ibz_k_points(self, runtype, test_config):
         test_property = "ibz_k_points"
         parser, reference = self._get_parser_and_reference(runtype, test_config, test_property)
-        if reference != "NOT_TESTED":
-            result = parser.ibz_k_points()
-            self.assertDeepAlmostEqual(reference, result)
+        result = parser.ibz_k_points()
+        self.assertDeepAlmostEqual(reference, result)
 
     @legacy_only
     def test_convergence_electronic(self, runtype, test_config):
         test_property = "convergence_electronic"
         parser, reference = self._get_parser_and_reference(runtype, test_config, test_property)
-        if reference != "NOT_TESTED":
-            result = parser.convergence_electronic()
-            self.assertDeepAlmostEqual(reference, result)
+        result = parser.convergence_electronic()
+        self.assertDeepAlmostEqual(reference, result)
 
     @legacy_only
     def test_convergence_ionic(self, runtype, test_config):
         test_property = "convergence_ionic"
         parser, reference = self._get_parser_and_reference(runtype, test_config, test_property)
-        if reference != "NOT_TESTED":
-            result = parser.convergence_ionic()
-            self.assertDeepAlmostEqual(reference, result)
+        result = parser.convergence_ionic()
+        self.assertDeepAlmostEqual(reference, result)
 
     @legacy_only
     def test_stress_tensor(self, runtype, test_config):
         test_property = "stress_tensor"
         parser, reference = self._get_parser_and_reference(runtype, test_config, test_property)
-        if reference != "NOT_TESTED":
-            result = parser.stress_tensor()
-            self.assertDeepAlmostEqual(reference, result)
+        result = parser.stress_tensor()
+        self.assertDeepAlmostEqual(reference, result)
 
     @for_all_espresso
     def test_pressure(self, runtype, test_config):
         test_property = "pressure"
         parser, reference = self._get_parser_and_reference(runtype, test_config, test_property)
-        if reference != "NOT_TESTED":
-            result = parser.pressure()
-            self.assertEqual(reference, result)
+        result = parser.pressure()
+        self.assertEqual(reference, result)
 
     @for_all_espresso
     def test_final_realspace_lattice_vectors(self, runtype, test_config):
         test_property = "realspace_lattice"
         parser, reference = self._get_parser_and_reference(runtype, test_config, test_property)
-        if reference != "NOT_TESTED":
-            result = parser.final_lattice_vectors()
-            self.assertDeepAlmostEqual(expected=reference, actual=result, places=3)
+        result = parser.final_lattice_vectors()
+        self.assertDeepAlmostEqual(expected=reference, actual=result, places=3)
 
     @for_all_espresso
     def test_final_reciprocal_lattice_vectors(self, runtype, test_config):
         test_property = "reciprocal_lattice"
         parser, reference = self._get_parser_and_reference(runtype, test_config, test_property)
-        if reference != "NOT_TESTED":
-            result = parser.xml_parser.final_lattice_vectors(reciprocal=True)
-            self.assertDeepAlmostEqual(expected=reference, actual=result)
+        result = parser.xml_parser.final_lattice_vectors(reciprocal=True)
+        self.assertDeepAlmostEqual(expected=reference, actual=result)
 
     @for_all_espresso
     def test_total_force(self, runtype, test_config):
         test_property = "total_force"
         parser, reference = self._get_parser_and_reference(runtype, test_config, test_property)
-        if reference != "NOT_TESTED":
-            result = parser.total_force()
-            self.assertEqual(reference, result)
+        result = parser.total_force()
+        self.assertEqual(reference, result)
 
     @legacy_only
     def test_atomic_forces(self, runtype, test_config):
         test_property = "atomic_forces"
         parser, reference = self._get_parser_and_reference(runtype, test_config, test_property)
-        if reference != "NOT_TESTED":
-            result = parser.atomic_forces()
-            self.assertDeepAlmostEqual(reference, result)
+        result = parser.atomic_forces()
+        self.assertDeepAlmostEqual(reference, result)
 
     @legacy_only
     def test_total_energy_contributions(self, runtype, test_config):
         test_property = "total_energy_contributions"
         parser, reference = self._get_parser_and_reference(runtype, test_config, test_property)
-        if reference != "NOT_TESTED":
-            result = parser.total_energy_contributions()
-            self.assertDeepAlmostEqual(reference, result)
+        result = parser.total_energy_contributions()
+        self.assertDeepAlmostEqual(reference, result)
 
     # =============================================
     # Legacy test cases that had specific fixtures
