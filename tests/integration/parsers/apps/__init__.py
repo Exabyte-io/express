dc7609ef4276b083d42e0ef92497e4f5ec437183
James Dean <24254612+AcylSilane@users.noreply.github.com>
2021-12-15 18:43:48 -0800

    test(espresso): Try again with test generation

--- tests/integration/parsers/apps/__init__.py
+++ tests/integration/parsers/apps/__init__.py
@@ -2,32 +2,58 @@ import copy
 import functools
 import operator
 import os
+import unittest
 from typing import List, Dict, Union
 
-from express.parsers.apps.espresso.parser import EspressoParser
+from express.parsers.apps.espresso.parser import EspressoLegacyParser
 from express.parsers.apps.nwchem.parser import NwchemParser
 from express.parsers.apps.vasp.parser import VaspParser
 from tests.integration import IntegrationTestBase
 import tests
 
 
-class ApplicationTestsBase(IntegrationTestBase):
-    application_name = None
+def testApplicationParsers(self):
+    if self.application_name:
+        versions = self.manifest['applications'][self.application_name]
+        for version, fixtures in versions.items():
+
+            # Loop over fixtuers
+            for fixture in fixtures:
+                work_dir = os.path.join(self.rootDir, 'fixtures', self.application_name, version, fixture['path'])
+                stdout_file = fixture['filename']
+                assert os.path.exists(os.path.join(work_dir, stdout_file))
+                # Loop over tests
+                for property_name, assertion, places in map(generate_test_config, fixture['tests']):
+
+                    with self.subTest(version=version,
+                                      property=property_name,
+                                      fixture=os.path.join(fixture['path'], stdout_file)):
+                        parser = self.parser(work_dir = work_dir, stdout_file = stdout_file)
+                        result_fun = getattr(parser, property_name)
+                        result = result_fun()
+                        print(result)
+
+                    # for test_property, test_settings in self.manifest['tests'].items():
+                    #     default_comparison_operator = self.__getattribute__(
+                    #         test_settings.get('comparison', 'assertDeepAlmostEqual'))
+                    #     default_precision = test_settings.get("places", 2)
 
 
-class EspressoTestBase(ApplicationTestsBase):
+class EspressoTestBase(IntegrationTestBase):
     application_name = "espresso"
-    parser = EspressoParser
+    parser = EspressoLegacyParser
+    testEspressoParser = testApplicationParsers
 
-
-class VaspTestBase(ApplicationTestsBase):
+class VaspTestBase(IntegrationTestBase):
     application_name = "vasp"
     parser = VaspParser
+    testVaspParser = testApplicationParsers
 
 
-class NWChemTestBase(ApplicationTestsBase):
+class NWChemTestBase(IntegrationTestBase):
     application_name = "nwchem"
     parser = NwchemParser
+    testNWChemParser = testApplicationParsers
 
 
 def get_application_config(cls):
@@ -51,7 +77,6 @@ def create_test(cls,
         def fun(self):
             parser = self.parser(work_dir=fixture_dir, stdout_file=stdout_file)
             result_fun = operator.attrgetter(property_to_test)(parser)
-
 
         fun_name = f"test_{cls.application_name}_{version}_{property_to_test}".replace(".", "-")
 
@@ -81,13 +106,13 @@ def _(test: str):
     places = default_test_config.get("places", 2)
     return property_to_test, comparison, places
 
-
-for cls in (NWChemTestBase, EspressoTestBase, VaspTestBase):
-    application_config = get_application_config(cls)
-
-    for version, configs in application_config.items():
-        for config in configs:
-            work_dir = config['path']
-            stdout_file = config['filename']
-            properties_to_test = config['tests']
-            create_test(cls, version, work_dir, stdout_file, properties_to_test)
+#
+# for cls in (NWChemTestBase, EspressoTestBase, VaspTestBase):
+#     application_config = get_application_config(cls)
+#
+#     for version, configs in application_config.items():
+#         for config in configs:
+#             work_dir = config['path']
+#             stdout_file = config['filename']
+#             properties_to_test = config['tests']
+#             create_test(cls, version, work_dir, stdout_file, properties_to_test)
