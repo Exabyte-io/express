2aae4c3eea172375114929644b44e7060a3c86c6
Alexander Zech <alexzech@gmx.de>
2022-10-28 16:39:58 -0700

    test: adjust test data

--- tests/unit/properties/non_scalar/test_averaged_potential.py
+++ tests/unit/properties/non_scalar/test_averaged_potential.py
@@ -7,10 +7,11 @@ from express.properties.non_scalar.averaged_potential import AveragedPotential
 AVG_POTENTIAL = {
     "name": "averaged_electrostatic_potential",
     "macroscopicMinima": {
-        "xDataArray": [1.6671738346873421],
-        "yDataSeries": [[-13.605157885039322]],
+        "xDataArray": [3.15050167],
+        "yDataSeries": [[-0.99996031]],
     }
 }
+
 
 class AveragedPotentialTest(UnitTestBase):
     def setUp(self):
@@ -22,9 +23,9 @@ class AveragedPotentialTest(UnitTestBase):
     def create_mock_data(self):
         """
         Creates mock potential data based on cosine in the range [0, 2*pi].
-        Minimum is at
-          x   = pi/2 * bohr_to_angstrom = 1.667
-          m_x = -1 * rydberg_to_ev      = -13.605
+        Exact minimum is at
+          x   = pi
+          m_x = -1
         """
         x = np.linspace(0, 6.28, 300)
         p_x = np.zeros(300)  # ignored for now
@@ -35,6 +36,6 @@ class AveragedPotentialTest(UnitTestBase):
     def test_averaged_potential(self):
         averaged_potential_data = self.create_mock_data()
         parser = MagicMock()
-        parser.attach_mock(MagicMock(return_value=averaged_potential_data), "averaged_quantity")
+        parser.attach_mock(MagicMock(return_value=averaged_potential_data), "averaged_potential")
         property_ = AveragedPotential("averaged_electrostatic_potential", parser)
         self.assertDeepAlmostEqual(property_.serialize_and_validate(), AVG_POTENTIAL)
