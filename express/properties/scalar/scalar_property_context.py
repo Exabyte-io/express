4a90dfd92a986c8aeaf37f088be3d87d8a936078
Alexander Zech <alexzech@gmx.de>
2022-11-01 17:08:23 -0700

    chore: add scalar property derived from context

---
+++ express/properties/scalar/scalar_property_context.py
@@ -0,0 +1,15 @@
+from typing import Type, Any
+from express.properties.scalar import ScalarProperty
+from express.parsers import BaseParser
+
+
+class ScalarPropertyFromContext(ScalarProperty):
+
+    def __init__(self,
+                 name: str,
+                 parser: Type[BaseParser],
+                 value: Any,
+                 *args,
+                 **kwargs):
+        super().__init__(name, parser, *args, **kwargs)
+        self.value = value
