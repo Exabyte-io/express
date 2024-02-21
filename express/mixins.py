6bc9b64d1765d793b7b2f9d8b1d9bae5529bc3c3
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-02-20 21:40:52 -0800

    update: round materials coordinates, lattice params x 2

--- express/mixins.py
+++ express/mixins.py
@@ -3,5 +3,6 @@ from express import settings
 
 
 class RoundNumericValuesMixin(object):
-    def _round(self, array):
-        return np.round(array, settings.PRECISION).tolist()
+
+    def _round(self, array, decimal_places=settings.PRECISION):
+        return np.round(array, decimal_places).tolist()
