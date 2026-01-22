2b5a245107d319d6947e6d1d5c137d0cea56cb59
VsevolodX <vsevolodbiryukov@icloud.com>
2026-01-22 12:53:15 -0800

    update: test with conversion

--- tests/unit/properties/non_scalar/two_dimensional_plot/test_wavefunction_amplitude.py
+++ tests/unit/properties/non_scalar/two_dimensional_plot/test_wavefunction_amplitude.py
@@ -1,18 +1,25 @@
 from tests.unit import UnitTestBase
+from express.parsers.settings import Constant
 from express.properties.non_scalar.two_dimensional_plot.wavefunction_amplitude import WavefunctionAmplitude
 
-
-WAVEFUNCTION_AMPLITUDE_RAW_DATA = [
+RAW_DATA_ALAT = [
     [0.0, 0.0050251256, 0.0100502513, 0.0150753769, 0.0201005025],
     [0.0000322091, 0.0000072134, -0.0000218274, -0.0000540398, -0.0000883573]
 ]
 
-WAVEFUNCTION_AMPLITUDE = {
+ALAT_BOHR = 10.0
+
+CONVERTED_DATA_ANGSTROMS = [
+    [x * ALAT_BOHR * Constant.BOHR for x in RAW_DATA_ALAT[0]],
+    RAW_DATA_ALAT[1]
+]
+
+EXPECTED = {
     "name": "wavefunction_amplitude",
-    "xDataArray": [0.0, 0.0050251256, 0.0100502513, 0.0150753769, 0.0201005025],
-    "yDataSeries": [[0.0000322091, 0.0000072134, -0.0000218274, -0.0000540398, -0.0000883573]],
+    "xDataArray": CONVERTED_DATA_ANGSTROMS[0],
+    "yDataSeries": [CONVERTED_DATA_ANGSTROMS[1]],
     "xAxis": {"label": "coordinate", "units": "angstrom"},
-    "yAxis": {"label": "amplitude", "units": "a.u."},
+    "yAxis": {"label": "amplitude"},
 }
 
 
@@ -24,7 +31,7 @@ class WavefunctionAmplitudeTest(UnitTestBase):
         super().tearDown()
 
     def test_wavefunction_amplitude(self):
-        parser = self.get_mocked_parser("wavefunction_amplitude", WAVEFUNCTION_AMPLITUDE_RAW_DATA)
+        parser = self.get_mocked_parser("wavefunction_amplitude", CONVERTED_DATA_ANGSTROMS)
         property_ = WavefunctionAmplitude("wavefunction_amplitude", parser)
-        self.assertDeepAlmostEqual(property_.serialize_and_validate(), WAVEFUNCTION_AMPLITUDE)
+        self.assertDeepAlmostEqual(property_.serialize_and_validate(), EXPECTED)
 
